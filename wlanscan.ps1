647df002cc0f73dbaec8afec3bc7d6bf46680ca3
Patrick Lang <patrick.lang@hotmail.com>
2016-08-24 19:42:46 -0700

    Add class, return object per BSSID

--- wlanscan.ps1
+++ wlanscan.ps1
@@ -12,6 +12,34 @@
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-
 PARAM ($ifname = "")
 
+class NetworkListEntry
+{
+    [string]$SSID
+    [string]$NetType
+    [string]$Auth
+    [string]$Encryption
+    [string]$BSSID
+    [string]$Signal
+    [string]$Radiotype
+    [string]$Channel
+
+}
+
+function CloneEntry
+{
+    param([NetworkListEntry]$in)
+    $obj = New-Object -TypeName NetworkListEntry
+    $obj.SSID = $in.SSID
+    $obj.NetType = $in.NetType
+    $obj.Auth = $in.Auth
+    $obj.Encryption = $in.Encryption
+    $obj.BSSID = $in.BSSID
+    $obj.Signal = $in.Signal
+    $obj.Radiotype = $in.Radiotype
+    $obj.Channel = $in.Channel
+    $obj
+}
+
 # Windows Vista/2008/7
 if  ((gwmi win32_operatingsystem).Version.Split(".")[0] -gt 6) {
 	throw "This script works on Windows Vista or higher."
@@ -30,20 +58,24 @@ $line = 0
 $ssidcount=0
 while ($line -lt $output.length) {
 	$currentline = $output[$line];
+    #Write-Host "Parsing " $currentline
 	if ($currentline -match "Interface") {
 		$CurrentIfName = [regex]::match($currentline.Replace("Interface name : ","")
 			                            ,"\w{1,}").ToString();
 	    if (($CurrentIfName.ToLower() -eq $ifname.ToLower()) -or ($ifname.length -eq 0)) {
 		    $iftest=$true;
 		} else { $iftest=$false }
-	}	 
+	}
 	
 	$buf = [regex]::replace($currentline,"[ ]","");
 	if ([regex]::IsMatch($buf,"^SSID\d{1,}(.)*") -and $iftest) {
-	   	$item = "" | Select-Object SSID,NetType,Auth,Encryption,BSSID,Signal,Radiotype,Channel;
-		$n+=1;
-       	$item.SSID = [regex]::Replace($buf,"^SSID\d{1,}:","");
+	   	#$item = "" | Select-Object SSID,NetType,Auth,Encryption,BSSID,Signal,Radiotype,Channel;
+        
+        $item = New-Object -TypeName NetworkListEntry
+		$item.SSID = [regex]::Replace($buf,"^SSID\d{1,}:","");
 		$ActiveNetworks+=$item;
+        $n = $ActiveNetworks.Length - 1
+		$ssidcount = 0;
 	}
   	if ([regex]::IsMatch($buf,"Networktype") -and $iftest) {
 	   	$ActiveNetworks[$n].NetType=$buf.Replace("Networktype:","");
@@ -53,9 +85,14 @@ while ($line -lt $output.length) {
 	}
 	if ([regex]::IsMatch($buf,"Encryption") -and $iftest) {
 	   	$ActiveNetworks[$n].Encryption=$buf.Replace("Encryption:","");
-	 	}
-        if ([regex]::IsMatch($buf,"BSSID\d") -and $iftest) {
-	   	$ActiveNetworks[$n].BSSID=[regex]::Replace($buf,"BSSID\d{1,}:","");
+	}
+    if ([regex]::IsMatch($buf,"BSSID\d") -and $iftest) {
+		if ($ssidcount -gt 0) { # if already have one BSSID for this SSID, copy the previous one
+			$ActiveNetworks += CloneEntry($ActiveNetworks[$n])
+			$n += 1;
+		}
+		$ActiveNetworks[$n].BSSID=[regex]::Replace($buf,"BSSID\d{1,}:","");
+		$ssidcount += 1;
 	}
 	if ([regex]::IsMatch($buf,"Signal") -and $iftest) {
 	   	$ActiveNetworks[$n].Signal=[int]::Parse([regex]::Match($buf.Replace("Signal:",""), "\d+")) / 100;
